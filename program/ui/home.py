315cb212b7b57a636615c108a5d358f9ea38a6ff
Ertibus <gameciopath@gmail.com>
2021-10-27 17:18:06 +0300

    feat: button panel template

--- program/ui/home.py
+++ program/ui/home.py
@@ -8,7 +8,30 @@ class Home():
         self.init_ui()
 
     def init_ui(self):
-        wrapper = QVBoxLayout()
+        wrapper = QGridLayout()
+        wrapper.setColumnStretch(1, 1)
         self.body.addLayout(wrapper)
 
-        wrapper.addWidget(QLabel("Hello World"))
+        button_layout = QVBoxLayout()
+
+        btn_other = QPushButton("other")
+        button_layout.addWidget(btn_other)
+
+        button_layout.addStretch()
+
+        btn_quit = QPushButton("Quit")
+        button_layout.addWidget(btn_quit)
+
+        event_layout = QVBoxLayout()
+        event_layout.addWidget(QLabel("Hello there traveler!"))
+        event_layout.addStretch()
+
+
+        button_box = QGroupBox(" ")
+        button_box.setLayout(button_layout)
+
+        event_box = QGroupBox(" ")
+        event_box.setLayout(event_layout)
+
+        wrapper.addWidget(button_box, 0, 0, 1, 1)
+        wrapper.addWidget(event_box, 0, 1, 1, 1)
